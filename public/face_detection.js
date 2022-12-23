a794526b4b8bc1c5096aed8fe94f50018662f58c
Edward <edi.iordache99@gmail.com>
2022-12-23 15:00:26 +0100

    Updated face expression detection script

--- public/face_detection.js
+++ public/face_detection.js
@@ -5,10 +5,11 @@ const MODEL_URL = '/models/face';
 
 Promise.all([
     // tinyfacedetector is just like a normal face detector but it's going to be smaller and quicker
-    faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+    // faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
     faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
     faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
     faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+    faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
 ]).then(addVideoListener);
 
 function addVideoListener(){
@@ -18,12 +19,12 @@ function addVideoListener(){
         const displaySize = { width: video.offsetWidth, height: video.offsetHeight };
         faceapi.matchDimensions(canvas, displaySize);
         setInterval(async () => {
-            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+            const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options()).withFaceExpressions();
             const resizedDetections = faceapi.resizeResults(detections, displaySize);
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
             faceapi.draw.drawDetections(canvas, resizedDetections);
             faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-        }, 100)
+        }, 500)
     })
 }
       
