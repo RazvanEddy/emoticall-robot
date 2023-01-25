38a8c6010fcfc4eecf70c4c3f40ce4e578eef030
Edward <edi.iordache99@gmail.com>
2023-01-25 13:00:32 +0100

    Added Meyda Analyzer

---
+++ public/voice_emotions.js
@@ -0,0 +1,33 @@
+navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia || navigator.mediaDevices.getUserMedia
+
+let analyzer = null;
+
+function onClickStartAnalyzer() {
+    setTimeout(
+        function startAnalyzer(){
+            const AudioContext = window.AudioContext || window.webkitAudioContext;
+            const audioContext = new AudioContext({ sampleRate: 22050 });
+            const voiceVideo = document.getElementById("localVideo");
+            const audioStream = voiceVideo.captureStream();
+            const source = audioContext.createMediaStreamSource(audioStream);
+            analyzer = Meyda.createMeydaAnalyzer({
+                audioContext: audioContext,
+                source: source,
+                bufferSize: 512,
+                featureExtractors: ["rms"],
+                sampleRate: 22050,
+                hopSize: 512,
+                windowingFunction: "hanning",
+                callback: features => {
+                    console.log(features);
+                },
+            });
+            analyzer.start();  
+        }, 5000);
+}
+
+function stopAnalyzer(){
+    analyzer.stop();
+    analyzer = null;
+}
+
